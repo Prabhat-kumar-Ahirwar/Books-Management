088705460c8ff759c5ca7201ec87158d402f7e3b
Prabhat-kumar-Ahirwar <prabhatmmg007@gmail.com>
2026-03-11 18:53:39 +0530

    Adding validation on Entity

--- src/main/java/com/example/BookManagementApp/Entity/Books.java
+++ src/main/java/com/example/BookManagementApp/Entity/Books.java
@@ -1,9 +1,14 @@
 package com.example.BookManagementApp.Entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "books")
@@ -15,7 +20,22 @@ public class Books {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @NotBlank(message = "Genre is required")
     private String genre;
+
+    @NotBlank(message = "Author is required")
     private String author;
+
+    @Positive(message = "Price must be more than 0")
+    private double price;
+
+    @Min(value = 0, message = "Stocks can not be negative")
+    private int stock;
+    private LocalDate publishDate;
+    @NotBlank(message = "Publisher is required")
+    private String publisher;
 }
